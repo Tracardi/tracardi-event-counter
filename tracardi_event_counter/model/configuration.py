c74224aac5ad4d55bd7bf7843c91552f01da904a
risto <risto.kowaczewski@gmail.com>
2021-10-25 14:52:09 +0200

    Event counter documentation.

--- tracardi_event_counter/model/configuration.py
+++ tracardi_event_counter/model/configuration.py
@@ -1,13 +1,17 @@
-from pydantic import BaseModel
+from typing import Union
+
+from pydantic import BaseModel, validator
 from pytimeparse import parse
 
 
 class Configuration(BaseModel):
     event_type: str
-    time_span: str
+    time_span: Union[str, int]
 
-    def get_time_span(self) -> int:
-        result = parse(self.time_span.strip("-"))
-        if result is None:
-            raise ValueError("Could not parse {} as time span".format(self.time_span))
-        return int(result)
+    @validator("time_span")
+    def valid_time_span(cls, value):
+        parsed_value = parse(value.strip("-"))
+        if parsed_value is None:
+            raise ValueError("Could not parse {} as time span".format(value))
+        return parsed_value
+
--- tracardi_event_counter/plugin.py
+++ tracardi_event_counter/plugin.py
@@ -1,23 +1,24 @@
 from tracardi.service.storage.driver import storage
 from tracardi_plugin_sdk.action_runner import ActionRunner
-from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData
+from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData, Form, FormGroup, FormField, FormComponent
 from tracardi_plugin_sdk.domain.result import Result
 
-
 from tracardi_event_counter.model.configuration import Configuration
+
+
+def validate(config: dict) -> Configuration:
+    return Configuration(**config)
 
 
 class EventCounter(ActionRunner):
 
     def __init__(self, **kwargs):
-        self.config = Configuration(**kwargs)
+        self.config = validate(kwargs)
 
     async def run(self, payload) -> Result:
-        print(self.config.event_type,
-            self.config.get_time_span())
         no_of_events = await storage.driver.event.count_events_by_type(
             self.config.event_type,
-            self.config.get_time_span()
+            self.config.time_span
         )
         return Result(port="payload", value={"events": no_of_events})
 
@@ -30,13 +31,40 @@ def register() -> Plugin:
             className='EventCounter',
             inputs=["payload"],
             outputs=['payload'],
-            version='0.1.2',
+            version='0.6.0',
             license="MIT",
             author="Dawid Kruk",
+            manual="event_counter_action",
             init={
                 "event_type": "",
                 "time_span": "-15m"
-            }
+            },
+            form=Form(
+                groups=[
+                    FormGroup(
+                        name="Event counter settings",
+                        description="Event counter reads how many events of defined type were triggered "
+                                    "within defined time.",
+                        fields=[
+                            FormField(
+                                id="event_type",
+                                name="Event type",
+                                description="Type event you would like to count.",
+                                component=FormComponent(type="text", props={
+                                    "label": "Event type"
+                                })
+                            ),
+                            FormField(
+                                id="time_span",
+                                name="Time span",
+                                description="Type time span, e.g. -15minutes.",
+                                component=FormComponent(type="text", props={
+                                    "label": "Time span"
+                                })
+                            ),
+                        ]
+                    )
+                ])
         ),
         metadata=MetaData(
             name='Event counter',
